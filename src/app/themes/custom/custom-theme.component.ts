ba4d53885205a676751e26195bd8b941e15ac499
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-05-18 23:25:27 +0200

    color picker

--- src/app/themes/custom/custom-theme.component.ts
+++ src/app/themes/custom/custom-theme.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef } from "@angular/core";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Inject, OnInit } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { ColorRgb, ColorRgbIf } from "@guiexpert/table";
 
 @Component({
   selector: "app-custom-theme",
@@ -6,16 +8,44 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef } fro
   styleUrls: ["./custom-theme.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CustomThemeComponent {
+export class CustomThemeComponent implements OnInit {
 
   public theme = "light";
   public light = true;
+  public arr = [
+    ["99%", 0.05],
+    ["96%", 0.05],
+    ["93%", 0.05],
+    ["90%", 0.1],
+    ["86%", 0.2],
+    ["83%", 0.2],
+    ["80%", 0.2],
+    ["75%", 0.2],
+    ["70%", 0.25],
+    ["65%", 0.31],
+    ["60%", 0.27],
+    ["55%", 0.27],
+    ["50%", 0.27],
+    ["45%", 0.25],
+    ["40%", 0.25],
+    ["35%", 0.25],
+    ["30%", 0.2],
+    ["25%", 0.2],
+    ["20%", 0.2],
+    ["15%", 0.2],
+    ["10%", 0.2],
+    ["5%", 0.1]
+  ];
+  public cols: number[] = [];
 
   constructor(
-    // @Inject(DOCUMENT) private readonly document: Document,
+    @Inject(DOCUMENT) private readonly document: Document,
     private readonly elementRef: ElementRef,
     private readonly cdr: ChangeDetectorRef
   ) {
+    for (let i = 0; i < 360; i++) {
+      this.cols.push(i);
+    }
   }
 
   private _value = 320;
@@ -31,7 +61,7 @@ export class CustomThemeComponent {
 
   setTheme(m: "light" | "dark") {
     this.theme = m;
-    this.light = m==='light';
+    this.light = m === "light";
     this.cdr.detectChanges();
   }
 
@@ -41,5 +71,42 @@ export class CustomThemeComponent {
 
   onModelChanged($event: any) {
     console.info($event);
+  }
+
+  ngOnInit(): void {
+    this.document.querySelectorAll(".swatch")
+      .forEach(ele =>
+        ele.addEventListener("click", evt => {
+          const bg = window.getComputedStyle(ele).getPropertyValue("background-color");
+          console.info("bg", bg);
+        })
+      );
+    this.document.querySelector(".oklch-picker");
+    addEventListener("click", evt => {
+      console.info(evt.target);
+      const h = (evt.target as HTMLDivElement).getAttribute("data-h");
+      // const bg = window.getComputedStyle(ele).getPropertyValue("background-color");
+      console.info("h", h);
+      if (h) {
+        this.value = parseInt(h);
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  rgb2hex(red: number, green: number, blue: number): string {
+    return "#" + red.toString(16) + green.toString(16) + blue.toString(16);
+  }
+
+  hexToRgb(hex: string): ColorRgbIf {
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? new ColorRgb(
+      parseInt(result[1], 16),
+      parseInt(result[2], 16),
+      parseInt(result[3], 16)) : new ColorRgb(0, 0, 0);
   }
 }
