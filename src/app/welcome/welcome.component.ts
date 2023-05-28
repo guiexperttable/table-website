e51086ecb8d96700154b87a2bdde58eb1ba4027c
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-05-28 12:11:50 +0200

    welcome

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef } from "@angular/core";
 
 @Component({
   selector: "app-doc-welcome",
@@ -7,5 +7,20 @@ import { ChangeDetectionStrategy, Component } from "@angular/core";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WelcomeComponent {
+
   freezeUi = true;
+
+  constructor(
+    private readonly elementRef: ElementRef
+  ) {
+  }
+
+  scrollDown() {
+    const h = document.body.offsetHeight;
+    this.elementRef.nativeElement.scrollBy({
+      top: h,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
 }
