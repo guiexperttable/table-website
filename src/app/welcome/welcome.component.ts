bf21309f1e9e01d33def233614522c36e4ba9b28
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-06-06 17:17:34 +0200

    new design

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, ElementRef } from "@angular/core";
+import { ChangeDetectionStrategy, Component, ElementRef, Inject, OnDestroy, OnInit } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "app-doc-welcome",
@@ -6,20 +7,22 @@ import { ChangeDetectionStrategy, Component, ElementRef } from "@angular/core";
   styleUrls: ["./welcome.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit, OnDestroy {
 
-  freezeUi = true;
+  public freezeUi = true;
+  private observer?: IntersectionObserver;
 
   constructor(
-    private readonly elementRef: ElementRef
+    private readonly elementRef: ElementRef,
+    @Inject(DOCUMENT) private readonly document: Document
   ) {
-    let nativeElement = this.elementRef.nativeElement;
-    nativeElement.addEventListener("scroll", () => {
-      const mh = nativeElement.scrollTop / (nativeElement.scrollHeight - nativeElement.offsetHeight);
-      const ph = (nativeElement.scrollTop & nativeElement.offsetHeight) / nativeElement.offsetHeight;
-      nativeElement.style.setProperty("--scroll-mh", mh + "");
-      nativeElement.style.setProperty("--scroll-ph", ph + "");
-    }, false);
+    // let nativeElement = this.elementRef.nativeElement;
+    // nativeElement.addEventListener("scroll", () => {
+    //   const mh = nativeElement.scrollTop / (nativeElement.scrollHeight - nativeElement.offsetHeight);
+    //   const ph = (nativeElement.scrollTop & nativeElement.offsetHeight) / nativeElement.offsetHeight;
+    //   nativeElement.style.setProperty("--scroll-mh", mh + "");
+    //   nativeElement.style.setProperty("--scroll-ph", ph + "");
+    // }, false);
   }
 
   scrollDown() {
@@ -37,5 +40,27 @@ export class WelcomeComponent {
       "http://localhost:4200/themes/custom/picker#welcome",
       "_blank",
       "left=100,top=100,width=720,height=755,location=0,scrollbars=0,status=0");
+  }
+
+  ngOnInit(): void {
+    this.observer = new IntersectionObserver(entries =>
+      entries
+        .forEach(entry => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add("show");
+            } else {
+              entry.target.classList.remove("show");
+            }
+          }
+        ));
+
+    this.document
+      .querySelectorAll("section")
+      .forEach(el => this.observer?.observe(el));
+
+  }
+
+  ngOnDestroy(): void {
+    this.observer?.disconnect();
   }
 }
