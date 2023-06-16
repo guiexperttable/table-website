af0bd0cc17ac086d23da57e8074bb102b407935a
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-06-16 21:22:02 +0200

    fix createHeatMapModel

--- src/app/demos/styledemo/heatmap/demo-heatmap.component.ts
+++ src/app/demos/styledemo/heatmap/demo-heatmap.component.ts
@@ -27,7 +27,7 @@ export class DemoHeatmapComponent {
   tableModel: TableModelIf = createHeatMapModel();
 
   constructor() {
-    console.info(this.tableModel)// tODO del
+    console.info(this.tableModel)// TODO del
   }
 }
 
